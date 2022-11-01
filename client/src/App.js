8fe2083b073804eab932800b58dbcd5850efb3cb
vaishnavihole <vaishnavihole1@gmail.com>
2022-11-01 07:27:27 +0530

    routing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter,Routes, Route,  } from 'react-router-dom';
+import Home from './views/Home/Home';
+import ChatPage from './views/ChatPage/ChatPage';
+import About from './views/About/About';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chatpage" element={<ChatPage/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

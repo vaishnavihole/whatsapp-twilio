08457c4875e3516ea4bd87ca0771a4c1d84d48ac
vaishnavihole <vaishnavihole1@gmail.com>
2022-11-15 09:22:41 +0530

    backend frontend intgration

--- client/src/views/ChatPage/ChatPage.js
+++ client/src/views/ChatPage/ChatPage.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import axios from 'axios'
 import './ChatPage.css'
 import ButtonImg from './sendbutton.png';
 import IncomingMessage from '../../components/IncomingMessage/IncomingMessage';
@@ -7,6 +8,17 @@ import OutgoingMessage from '../../components/OutgoingMessage/OutgoingMessage';
 
 
 function ChatPage() {
+   const [currentMessage, setCurrentMessage] = useState("")
+
+  async function sendMessage(){
+  const response = await  axios.post('/send',{
+      to: "919588603013",
+      text: currentMessage
+   })
+   console.log(response.data)
+    setCurrentMessage("")
+   }
+
   return (
     <div>
       <div className='chatpage-container'>
@@ -14,8 +26,8 @@ function ChatPage() {
         <OutgoingMessage />
       </div>
       <div className='mesaage-input-box-container'>
-        <input type="text" className='message-input-box' placeholder=' Type a  message' />
-        <img src={ButtonImg} alt="" />
+        <input type="text" value={currentMessage} onChange={(e)=>{setCurrentMessage(e.target.value)}} className='message-input-box' placeholder=' Type a  message' />
+        <img src={ButtonImg} onClick={sendMessage} alt="" />
 
       </div>
     </div>
